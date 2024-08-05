b8253d4e9ac58da3ee6f0619d3470a8eabdda468
Shaikh Juned <46794363+shaikhjunaid775@users.noreply.github.com>
2024-08-05 18:24:59 +0530

    Update Banner.jsx

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -7,7 +7,7 @@ function Banner() {
         <div className="container mx-auto flex md:px-24 md:py-10 md:flex-row flex-col items-center  h-screen">
           <div className="lg:flex-grow mt-5 md:mt-0   md:w-1.5/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="text-2xl font-extrabold leading-9 tracking-tight mb-3 text-gray-900  sm:text-4xl sm:leading-10 md:text-5xl md:leading-normal">
-              MediCare Hub
+              MediCare Hubs
             </h1>
             <p className="mb-8 md:pl-0  pl-2 pr-2 leading-relaxed ">
               Comprehensive React-based hospital management system for efficient
